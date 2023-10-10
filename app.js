326f0306bad025718a1764462368eed4aabf05cc
kartik satish devarde <kartikdevarde@outlook.com>
2023-10-10 10:56:38 +0530

    fix issus

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const path = require('path');
 
-const nfts = JSON.parse(fs.readFileSync(`${__dirname}/nft-data/data/nft-simple.json`));
+const filePath = path.join(__dirname, 'Api-starter-file-main', 'nft-data', 'data', 'nft-users.json');
+
+const nfts = JSON.parse(fs.readFileSync(filePath));
 
 app.get('/api/v1/device', (req, res) => {
     res.status(200).json({
         status: "success",
-        data: {}
+        data: { nfts }
     });
 });
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`App running on port ${port}...`);
+    console.log('App running on port ${port}...');
 });
